c8a19218531ab49fa3c44991317d64d0372f0e11
Julio Antonio Rubio Moreno <jrubiomo@everis.com>
2021-05-21 09:14:45 +0200

    delete port in main file

--- docker_server_dev/flask_app/main.py
+++ docker_server_dev/flask_app/main.py
@@ -20,4 +20,4 @@ app.config.from_object(config.Config)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(debug=True, host='0.0.0.0')
