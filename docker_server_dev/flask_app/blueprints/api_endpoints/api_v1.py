9cb64e2ca101ef8b44d440606c3c2b064a5b6852
Julio Antonio Rubio Moreno <jrubiomo@everis.com>
2021-05-23 13:00:07 +0200

    add put and delete methods, update get for one user

--- docker_server_dev/flask_app/blueprints/api_endpoints/api_v1.py
+++ docker_server_dev/flask_app/blueprints/api_endpoints/api_v1.py
@@ -1,3 +1,6 @@
+"""
+Users have to set headers with {Content-type: application/json} in order to work. A json is expected to get data.
+"""
 from flask import Blueprint, request, jsonify
 from flasgger import swag_from
 from DDBB.database import User
@@ -9,14 +12,29 @@ blueprint = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 @blueprint.route('/users', methods=['GET', 'POST'])
 @swag_from('/usr/src/app/swagger/users_doc_v1.yaml')
 def users():
+    db_user = User()
     if request.method == 'GET':
-        db_user = User()
-        users = db_user.get_users()
-        db_user.close()
-        return jsonify(users), 200
+        response_body, response_code = db_user.get_users()
     elif request.method == 'POST':
-        db_user = User()
-        response, message = db_user.set_user(request.json)
-        db_user.close()
-        response_code = 201 if response else 400
-        return jsonify(message), response_code
+        response_body, response_code = db_user.set_user(request.json)
+    else:
+        response_body = 'Unable to process'
+        response_code = 405
+    db_user.close()
+    return jsonify(response_body), response_code
+
+
+@blueprint.route('/users/<user_id>', methods=['GET', 'PUT', 'DELETE'])
+def user(user_id):
+    db_user = User()
+    if request.method == 'GET':
+        response_body, response_code = db_user.get_users(user_id)
+    elif request.method == 'PUT':
+        response_body, response_code = db_user.set_user_changes(user_id, request.json)
+    elif request.method == 'DELETE':
+        response_body, response_code = db_user.delete_user(user_id)
+    else:
+        response_body = 'Unable to process'
+        response_code = 405
+    db_user.close()
+    return jsonify(response_body), response_code
--- docker_server_dev/mysql/init_table.sql
+++ docker_server_dev/mysql/init_table.sql
@@ -4,7 +4,10 @@ USE multicont_db;
 
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name varchar(50) NOT NULL
+	name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	address varchar(50) NOT NULL,
+	phone varchar(10) NOT NULL
 );
 
 
